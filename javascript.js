c8ebb2cca2fc8f352703268f8177fc958f8d2702
Raymart Domagas <raymartdomagas@Raymarts-MacBook-Air.local>
2023-04-04 16:43:25 -0400

    feat: added javascript external file and adjusted home page.

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// use "hello world" tier javascript in page
+
+// alert that welcomes the user to my webpage
+let string= 'Hello, welcome to my webpage! Please click on the links to the right to navigate to the other pages'
+alert(string) 
